3fc2b0913374324ee132d6ee5f774feb9c695cc6
seulgi <asd0927122@gmail.com>
2023-06-07 14:22:38 +0900

    String roomType으로 변경

--- Reservation.java
+++ Reservation.java
@@ -1,17 +1,16 @@
 package hotelProject;
 
 public class Reservation extends Customer {
-    int room;
+    String roomType;
     String reservationDate;
 
 
     public  Reservation() {}
     public void getRoom() {}
     public void getReservationDate() {}
-    public Reservation(String name, String phoneNumber, int room, String reservationDate) {
+    public Reservation(String name, String phoneNumber, String roomType, String reservationDate) {
         super(name, phoneNumber);
-        this.room = room;
+        this.roomType = roomType;
         this.reservationDate = reservationDate;
     }
-
 }
